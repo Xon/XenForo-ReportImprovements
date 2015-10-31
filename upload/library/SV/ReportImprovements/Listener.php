3d21d4a78c1a308759275565f65ae12b15a13342
Xon <tazipper1@hotmail.com>
2015-10-31 20:36:04 +0800

    Show Report count if the user has permission

--- upload/library/SV/ReportImprovements/Listener.php
+++ upload/library/SV/ReportImprovements/Listener.php
@@ -16,4 +16,56 @@ class SV_ReportImprovements_Listener
         }
         $extend[] = self::AddonNameSpace.$class;
     }
+
+    public static function controller_pre_dispatch(XenForo_Controller $controller, $action, $controllerName)
+    {
+        if (!XenForo_Application::isRegistered('session'))
+        {
+            return;
+        }
+
+        $visitor = XenForo_Visitor::getInstance();
+        if ($visitor['is_moderator'])
+        {
+            return;
+        }
+
+        $reportModel = $controller->getModelFromCache('XenForo_Model_Report');
+        if (!$reportModel->canViewReports())
+        {
+            return;
+        }
+
+        $visitor['canViewReports'] = true;
+
+        if (XenForo_Application::isRegistered('reportCounts'))
+        {
+            $reportCounts = XenForo_Application::get('reportCounts');
+        }
+        else
+        {
+            $reportCounts = $reportModel->rebuildReportCountCache();
+        }
+
+        $session = XenForo_Application::get('session');
+        $sessionReportCounts = $session->get('reportCounts');
+
+        if (!is_array($sessionReportCounts) || $sessionReportCounts['lastBuildDate'] < $reportCounts['lastModifiedDate'])
+        {
+            if (!$reportCounts['activeCount'])
+            {
+                $sessionReportCounts = array(
+                    'total' => 0,
+                    'assigned' => 0
+                );
+            }
+            else
+            {
+                $sessionReportCounts = $reportModel->getActiveReportsCountsForUser();
+            }
+
+            $sessionReportCounts['lastBuildDate'] = XenForo_Application::$time;
+            $session->set('reportCounts', $sessionReportCounts);
+        }
+    }
 }
