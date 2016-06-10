0779208f22c50a16b228d0ae2f29fcbdd5cc4713
Xon <tazipper1@hotmail.com>
2016-06-11 06:58:21 +0800

    Fix 'optimize index' support.

--- upload/library/SV/ReportImprovements/XenES/Model/Elasticsearch.php
+++ upload/library/SV/ReportImprovements/XenES/Model/Elasticsearch.php
@@ -1,7 +1,30 @@
 <?php
 
-class SV_ReportImprovements_XenES_Model_ElasticSearch extends XFCP_SV_ReportImprovements_XenES_Model_ElasticSearch
+class SV_ReportImprovements_XenES_Model_Elasticsearch extends XFCP_SV_ReportImprovements_XenES_Model_Elasticsearch
 {
+	public function getOptimizableMappings(array $mappingTypes = null, $mappings = null)
+	{
+		if ($mappingTypes === null)
+		{
+			$mappingTypes = $this->getAllSearchContentTypes();
+		}
+		if ($mappings === null)
+		{
+			$mappings = $this->getMappings();
+		}
+        return $this->_getOptimizableMappings($mappingTypes, $mappings, []);
+	}
+
+	protected function _getOptimizableMappings(array $mappingTypes, $mappings, $extraMappings)
+	{
+        $extraMappings = array_merge($extraMappings, SV_ReportImprovements_Installer::$extraMappings);
+        if (is_callable('parent::_getOptimizableMappings'))
+        {
+            return parent::_getOptimizableMappings($mappingTypes, $mappings, $extraMappings);
+        }
+        return SV_Utils_ElasticSearch::getOptimizableMappings($mappingTypes, $mappings, $extraMappings);
+    }
+
     public function optimizeMapping($type, $deleteFirst = true, array $extra = array())
     {
         if (isset(SV_ReportImprovements_Installer::$extraMappings[$type]))
