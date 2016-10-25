177225d88cdc9bd355204afd530ea419b272c369
Xon <tazipper1@hotmail.com>
2016-10-25 09:18:31 +0800

    Fix SQL query overwriting the user_id of the report comment

--- upload/library/SV/ReportImprovements/XenForo/Model/Report.php
+++ upload/library/SV/ReportImprovements/XenForo/Model/Report.php
@@ -15,13 +15,10 @@ class SV_ReportImprovements_XenForo_Model_Report extends XFCP_SV_ReportImproveme
     {
         $db = $this->_getDb();
         return $this->fetchAllKeyed("
-            SELECT report_comment.*,
-                user.*
-                , IF(user.username IS NULL, report_comment.username, user.username) AS username
-                ,warning.warning_id
-                ,warningLog.*
-                ,warning.is_expired
-                ,liked_content.like_date
+            SELECT report_comment.*, 
+                warning.warning_id, warningLog.*, warning.is_expired,
+                liked_content.like_date,
+                user.*, IF(user.username IS NULL, report_comment.username, user.username) AS username
             FROM xf_report_comment AS report_comment
             LEFT JOIN xf_sv_warning_log warningLog on warningLog.warning_log_id = report_comment.warning_log_id
             LEFT JOIN xf_warning warning on warningLog.warning_id = warning.warning_id
@@ -37,10 +34,10 @@ class SV_ReportImprovements_XenForo_Model_Report extends XFCP_SV_ReportImproveme
     public function getReportCommentById($id)
     {
         return $this->_getDb()->fetchRow('
-            SELECT report_comment.*, user.*, IF(user.username IS NULL, report_comment.username, user.username) AS username
-                ,warning.warning_id
-                ,warningLog.*
-                ,warning.is_expired
+            SELECT report_comment.*, 
+                warning.warning_id, warningLog.*, warning.is_expired,
+                liked_content.like_date,
+                user.*, IF(user.username IS NULL, report_comment.username, user.username) AS username
             FROM xf_report_comment AS report_comment
             LEFT JOIN xf_user AS user ON (user.user_id = report_comment.user_id)
             LEFT JOIN xf_sv_warning_log warningLog on warningLog.warning_log_id = report_comment.warning_log_id
@@ -57,10 +54,10 @@ class SV_ReportImprovements_XenForo_Model_Report extends XFCP_SV_ReportImproveme
         }
 
         return $this->fetchAllKeyed('
-            SELECT report_comment.*, user.*, IF(user.username IS NULL, report_comment.username, user.username) AS username
-                ,warning.warning_id
-                ,warningLog.*
-                ,warning.is_expired
+            SELECT report_comment.*, 
+                warning.warning_id, warningLog.*, warning.is_expired,
+                liked_content.like_date,
+                user.*, IF(user.username IS NULL, report_comment.username, user.username) AS username
             FROM xf_report_comment AS report_comment
             LEFT JOIN xf_user AS user ON (user.user_id = report_comment.user_id)
             LEFT JOIN xf_sv_warning_log warningLog on warningLog.warning_log_id = report_comment.warning_log_id
