f2e139b02e7ccbae9330418f86a8261b34eab3c9
Xon <tazipper1@hotmail.com>
2017-04-19 08:56:17 +0800

    Trim thread title out of link.

--- upload/library/SV/ReportImprovements/XenForo/Model/Thread.php
+++ upload/library/SV/ReportImprovements/XenForo/Model/Thread.php
@@ -4,21 +4,22 @@ class SV_ReportImprovements_XenForo_Model_Thread extends XFCP_SV_ReportImproveme
 {
     public function _getReplyBanLogData(array $thread, array $replyBan, $expired = false)
     {
-        $link = XenForo_Link::buildPublicLink('full:threads', $thread);
+        $link = XenForo_Link::buildPublicLink('full:threads', array_merge($thread, array('title' => '')));
+
         return array(
-            'content_type'          => 'user',
-            'content_id'            => $replyBan['user_id'],
-            'content_title'         => $replyBan['username'],
-            'user_id'               => $replyBan['user_id'],
-            'warning_date'          => $replyBan['ban_date'],
-            'warning_user_id'       => $replyBan['ban_user_id'],
-            'title'                 => (string)new XenForo_Phrase('SV_Reply_Banned_From_Thread_title', array('thread' => $thread['title'])),
-            'reply_ban_thread_id'   => $thread['thread_id'],
-            'notes'                 => (string)new XenForo_Phrase('SV_Reply_Banned_notes', array('link' => $link, 'reason' => $replyBan['reason'])),
-            'points'                => 0,
-            'expiry_date'           => $replyBan['expiry_date'],
-            'is_expired'            => $expired,
-            'extra_user_group_ids'  => '',
+            'content_type' => 'user',
+            'content_id' => $replyBan['user_id'],
+            'content_title' => $replyBan['username'],
+            'user_id' => $replyBan['user_id'],
+            'warning_date' => $replyBan['ban_date'],
+            'warning_user_id' => $replyBan['ban_user_id'],
+            'title' => (string)new XenForo_Phrase('SV_Reply_Banned_From_Thread_title', array('thread' => $thread['title'])),
+            'reply_ban_thread_id' => $thread['thread_id'],
+            'notes' => (string)new XenForo_Phrase('SV_Reply_Banned_notes', array('link' => $link, 'reason' => $replyBan['reason'])),
+            'points' => 0,
+            'expiry_date' => $replyBan['expiry_date'],
+            'is_expired' => $expired,
+            'extra_user_group_ids' => '',
         );
     }
 
@@ -100,7 +101,7 @@ class SV_ReportImprovements_XenForo_Model_Thread extends XFCP_SV_ReportImproveme
         $replyBans = $db->fetchAll("
 			SELECT xf_thread_reply_ban.*, xf_user.username 
 			FROM xf_thread_reply_ban
-			left join xf_user on xf_user.user_id = xf_thread_reply_ban.user_id
+			LEFT JOIN xf_user ON xf_user.user_id = xf_thread_reply_ban.user_id
 			WHERE expiry_date <= ?
 		", $expiryDate);
         $operationType = SV_ReportImprovements_Model_WarningLog::Operation_ExpireWarning;
