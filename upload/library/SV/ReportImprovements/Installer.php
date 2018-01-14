73b0840aa238a89677396237ea302d012c8239c6
Xon <tazipper1@hotmail.com>
2018-01-14 14:23:18 +0800

    Enforce minimum php & XenForo version in installer

--- upload/library/SV/ReportImprovements/Installer.php
+++ upload/library/SV/ReportImprovements/Installer.php
@@ -5,6 +5,19 @@ class SV_ReportImprovements_Installer
     public static function install($installedAddon, array $addonData, SimpleXMLElement $xml)
     {
         $version = isset($installedAddon['version_id']) ? $installedAddon['version_id'] : 0;
+        $required = '5.4.0';
+        $phpversion = phpversion();
+        if (version_compare($phpversion, $required, '<'))
+        {
+            throw new XenForo_Exception(
+                "PHP {$required} or newer is required. {$phpversion} does not meet this requirement. Please ask your host to upgrade PHP",
+                true
+            );
+        }
+        if (XenForo_Application::$versionId < 1030070)
+        {
+            throw new XenForo_Exception('XenForo 1.3.0+ is Required!', true);
+        }
 
         $db = XenForo_Application::getDb();
 
